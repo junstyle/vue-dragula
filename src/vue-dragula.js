dc48cd8b22e7ed293e99057e55d68db8830fefff
junstyle <junstyle@git>
2020-05-28 15:09:11 +0800

    optimize code

--- src/vue-dragula.js
+++ src/vue-dragula.js
@@ -2,87 +2,87 @@ import dragula from 'dragula'
 import DragulaService from './service'
 
 if (!dragula) {
-  throw new Error('[vue-dragula] cannot locate dragula.')
+    throw new Error('[vue-dragula] cannot locate dragula.')
 }
 
 export default function (Vue) {
-  const service = new DragulaService(Vue)
+    const service = new DragulaService(Vue)
 
-  let name = 'globalBag'
-  let drake
+    let name = 'globalBag'
+    let drake
 
-  Vue.vueDragula = {
-    options: service.setOptions.bind(service),
-    find: service.find.bind(service),
-    eventBus: service.eventBus
-  }
-
-  Vue.directive('dragula', {
-
-    bind(container, binding, vnode) {
-      const bagName = vnode.data.attrs.bag
-
-      if (bagName !== undefined && bagName.length !== 0) {
-        name = bagName
-      }
-      const bag = service.find(name)
-      if (bag) {
-        drake = bag.drake
-        drake.containers.push(container)
-        return
-      }
-      drake = dragula({
-        containers: [container]
-      })
-      service.add(name, drake)
-
-      service.handleModels(name, drake)
-    },
-
-    update(container, binding, vnode, oldVnode) {
-      const newValue = binding.value
-      if (!newValue) { return }
-
-      const bagName = vnode.data.attrs.bag
-      if (bagName !== undefined && bagName.length !== 0) {
-        name = bagName
-      }
-      const bag = service.find(name)
-      drake = bag.drake
-      if (!drake.models) {
-        drake.models = []
-      }
-
-      let modelContainer = service.findModelContainerByContainer(container, drake)
-
-      if (modelContainer) {
-        modelContainer.model = newValue
-      } else {
-        drake.models.push({
-          model: newValue,
-          container: container
-        })
-      }
-    },
-
-    unbind(container, binding, vnode) {
-      let unbindBagName = 'globalBag'
-      const bagName = vnode.data.attrs.bag
-
-      if (bagName !== undefined && bagName.length !== 0) {
-        unbindBagName = bagName
-      }
-      var drake = service.find(unbindBagName).drake
-      if (!drake) { return }
-      var containerIndex = drake.containers.indexOf(container)
-      if (containerIndex > -1) {
-        drake.containers.splice(containerIndex, 1)
-      }
-      if (drake.containers.length === 0) {
-        service.destroy(unbindBagName)
-      }
+    Vue.vueDragula = {
+        options: service.setOptions.bind(service),
+        find: service.find.bind(service),
+        eventBus: service.eventBus
     }
 
-  })
+    Vue.directive('dragula', {
+
+        bind(container, binding, vnode) {
+            const bagName = vnode.data.attrs.bag
+
+            if (bagName !== undefined && bagName.length !== 0) {
+                name = bagName
+            }
+            const bag = service.find(name)
+            if (bag) {
+                drake = bag.drake
+                drake.containers.push(container)
+                drake.models.push({ model: binding.value, container })
+                return
+            }
+
+            drake = dragula({
+                containers: [container]
+            })
+            drake.models = [{ model: binding.value, container }]
+
+            service.add(name, drake)
+            service.handleModels(name, drake)
+        },
+
+        // update(container, binding, vnode) {
+        //     const newValue = binding.value
+        //     if (!newValue) return
+
+        //     const bagName = vnode.data.attrs.bag
+        //     if (bagName !== undefined && bagName.length !== 0) {
+        //         name = bagName
+        //     }
+        //     const bag = service.find(name)
+        //     drake = bag.drake
+
+        //     let modelContainer = service.findModelContainerByContainer(container, drake)
+
+        //     if (modelContainer) {
+        //         modelContainer.model = newValue
+        //     } else {
+        //         drake.models.push({
+        //             model: newValue,
+        //             container
+        //         })
+        //     }
+        // },
+
+        unbind(container, binding, vnode) {
+            let unbindBagName = 'globalBag'
+            const bagName = vnode.data.attrs.bag
+
+            if (bagName !== undefined && bagName.length !== 0) {
+                unbindBagName = bagName
+            }
+            var drake = service.find(unbindBagName).drake
+            if (!drake) return
+            var containerIndex = drake.containers.indexOf(container)
+            if (containerIndex > -1) {
+                drake.containers.splice(containerIndex, 1)
+            }
+            if (drake.containers.length === 0) {
+                service.destroy(unbindBagName)
+            }
+        }
+
+    })
 }
 
